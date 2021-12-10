fc075ae3bac5e3fcf8910f963df24841917ebc79
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2021-12-10 14:46:05 +0100

    Update index.js

--- src/Resources/js/index.js
+++ src/Resources/js/index.js
@@ -1,4 +1,4 @@
 import { fieldRegistry } from 'sulu-admin-bundle/containers';
-import ColorPickerCustom from './components/CustomToggle/CustomToggle';
+import CustomToggle from './components/CustomToggle/CustomToggle';
 
 fieldRegistry.add('custom_toggle', CustomToggle);
